8e8e20f79a663f4a8718197cfcad91f828b77c7b
Minh Dinh <minhdinhtranhoang@gmail.com>
2024-06-24 23:32:27 +0700

    feat: add token limit logic for buffer and long-term memory

--- machine/robot/engines/memory/buffer_memory.py
+++ machine/robot/engines/memory/buffer_memory.py
@@ -1,13 +1,36 @@
+from tokencost import count_string_tokens
+
 class BufferMemory(dict):
-    def __init__(self, memory_size=10) -> None:
+    def __init__(self, memory_size=1000) -> None:
         super().__init__({"pending_message": "", "chat_history": []})
         self.memory_size = memory_size
 
     def __call__(self) -> str:
         return self.format_chat_history()
+    
+    def set_token_limit(self, token: int) -> None:
+        self.token_limit = token
 
-    def format_chat_history(self):
-        return "\n".join([f"{msg['sender']}: {msg['message']}" for msg in self["chat_history"]])
+    def format_chat_history(self) -> str:
+        # Default to show memory_size most recent messages
+        if not hasattr(self, "token_limit"):
+            return "\n".join([f"{msg['sender']}: {msg['message']}" for msg in self["chat_history"][-self.memory_size :]])
+        
+        # Add more messages until token limit is reached
+        index = len(self["chat_history"]) - 1
+        chat_history = []
+        tot_token = 0
+        while index >= 0:
+            msg = self["chat_history"][index]
+            cur_chat_str = f"{msg['sender']}: {msg['message']}"
+            cur_chat_token = count_string_tokens(cur_chat_str)
+            if tot_token + cur_chat_token > self.token_limit:
+                break
+            index -= 1
+            chat_history.append(cur_chat_str)
+        chat_history.reverse()
+        return "\n".join(chat_history)
+        # return "\n".join([f"{msg['sender']}: {msg['message']}" for msg in self["chat_history"]])
 
     def clear_pending_message(self):
         self["pending_message"] = ""
--- machine/robot/engines/memory/long_term_memory.py
+++ machine/robot/engines/memory/long_term_memory.py
@@ -9,10 +9,12 @@ from sqlalchemy import select
 from core.db.session import DBType, sessions
 from machine.models import QueryLogs, ResponseLogs
 
+from tokencost import count_string_tokens
 
 class LongTermMemory:
-    def __init__(self, top_matches: Optional[int] = 10):
+    def __init__(self, top_matches: Optional[int] = 20, token_limit: Optional[int] = 1000) -> None:
         self._top_matches = top_matches
+        self.set_token_limit(token_limit)
 
     async def save_interaction(self, input: str, output: str, vectorized_input, vectorized_output) -> None:
         sessions[DBType.PGVECTOR].set_session_context(str(uuid4()))
@@ -26,34 +28,47 @@ class LongTermMemory:
             session.add(ResponseLogs(payload=json.dumps(payload), embedding=vectorized_output))
             await session.commit()
 
+    def set_token_limit(self, token: int) -> None:
+        self.token_limit = token
+
     async def similarity_search(self, vectorized_input) -> Dict[str, str]:
         res = {}
         res["long_term_memory"] = "## Past interaction:\n\n"
+        limit = self.token_limit
         # DB Session for similarity search
         sessions[DBType.PGVECTOR].set_session_context(str(uuid4()))
         async with sessions[DBType.PGVECTOR].session() as session:
             # Top self._top_matches similarity search neighbors from input and output tables
-            input_match = await session.scalars(
-                select(QueryLogs).order_by(QueryLogs.embedding.l2_distance(vectorized_input)).limit(self._top_matches)
-            )
-            output_match = await session.scalars(
-                select(ResponseLogs)
-                .order_by(ResponseLogs.embedding.l2_distance(vectorized_input))
-                .limit(self._top_matches)
-            )
+            while limit > 0:
+                input_match = await session.scalars(
+                    select(QueryLogs).order_by(QueryLogs.embedding.l2_distance(vectorized_input)).limit(self._top_matches)
+                )
+                output_match = await session.scalars(
+                    select(ResponseLogs)
+                    .order_by(ResponseLogs.embedding.l2_distance(vectorized_input))
+                    .limit(self._top_matches)
+                )
 
-            result = list(input_match) + list(output_match)
-            # Result sorted by l2 distance
-            ordered_res = sorted(result, key=lambda x: l2_distance(x.embedding - vectorized_input))[: self._top_matches]
+                result = list(input_match) + list(output_match)
+                # Result sorted by l2 distance
+                ordered_res = sorted(result, key=lambda x: l2_distance(x.embedding - vectorized_input))[: self._top_matches]
 
-            # Ordered result by time
-            ordered_res = sorted(
-                ordered_res, key=lambda x: datetime.strptime(json.loads(x.payload)["timestamp"], "%Y-%m-%d %H:%M:%S")
-            )
+                # Ordered result by time
+                ordered_res = sorted(
+                    ordered_res, key=lambda x: datetime.strptime(json.loads(x.payload)["timestamp"], "%Y-%m-%d %H:%M:%S")
+                )
 
-            for inter in ordered_res:
-                payload = json.loads(inter.payload)
-                res["long_term_memory"] += f'At {payload["timestamp"]}:\n'
-                res["long_term_memory"] += f'- Human: {payload["input"]}\n- AI: {payload["output"]}\n\n'
-            await session.commit()
+                for inter in ordered_res:
+                    payload = json.loads(inter.payload)
+                    entry = f'At {payload["timestamp"]}:\n- Human: {payload["input"]}\n- AI: {payload["output"]}\n\n'
+                    entry_token = count_string_tokens(entry)
+                    limit -= entry_token
+                    if limit < 0:
+                        break
+                    res["long_term_memory"] += entry
+                    
+                if len(ordered_res) == 0:
+                    res["long_term_memory"] += "No past interactions found\n\n"
+                    break
+                await session.commit()
         return res
