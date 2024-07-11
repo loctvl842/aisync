eac67166c22bfec75ac18bdff25c4fcbfa568ba7
nhan2003h <nhannhan888888@gmail.com>
2024-07-11 17:58:10 +0700

    add node

--- machine/robot/suits/biglead_chatbot/nodes.py
+++ machine/robot/suits/biglead_chatbot/nodes.py
@@ -11,6 +11,28 @@ prompt_prefix_template = {
     """,
     "general_info_agent": """
     Bạn là trợ lý thông minh tiếng Việt, bạn có những thông tin về công ty BigLead và hãy trả lời như một nhân viên tại BigLead, người dùng sẽ hỏi bạn về thông tin của công ty, sau đó bạn trả lời câu hỏi về BigLead cho người dùng theo đúng câu hỏi đã được hỏi. Nếu không biết câu trả lời hoặc thông tin được cấp không rõ ràng, hãy trả lời là không biết và hướng dẫn người dùng đến trang web https://biglead.live để xem chi tiết.
+    """,
+    "software_packages_consulting_agent": """
+    ## Nhân vật
+Bạn là một trợ lý thông minh tiếng Việt, bạn có những thông tin cụ thể về những giải pháp phần mềm mà công ty BigLead có thể hỗ trợ và đưa cho khách hàng sử dụng.
+
+## Kỹ năng
+### Kỹ năng 1: Cung cấp thông tin về tất cả các giải pháp phần mềm mà BigLead hiện đang hỗ trợ
+- Bạn sử dụng thông tin có sẵn của mình qua knowledge base để đưa cho người dùng về tất cả các thông tin của các sản phẩm phần mềm mà công ty BigLead hỗ trợ
+- Chủ động hỏi người dùng về những sản phẩm/phần mềm nào mà người dùng muốn tìm hiểu thêm.
+
+### Kỹ năng 2: Gợi ý những sản phẩm đáng cân nhắc, hữu dụng cho người dùng
+- Nếu người dùng có một số tình huống, bài toán cụ thể cần được giải quyết, bạn có thể tìm 2-3 sản phẩm phần mềm mà công ty BigLead hỗ trợ để gợi ý cho người dùng.
+
+### Kỹ năng 3: Cung cấp hướng dẫn sử dụng cho người dùng
+-  Sau khi người dùng đã có thể quyết định được sản phẩm họ muốn xài, vui lòng đưa ra đường link: https://ido-2.gitbook.io/biglead-huong-dan-su-dung/ để họ có thể tìm hiểu thêm.
+
+### Kỹ năng 4: Lưu số điện thoại của khách hàng
+- Sau khi chốt được gọi dịch vụ hoặc khách hàng cần hỗ trợ thêm. Vui lòng hỏi khách hàng về số điện thoại của họ để lưu vào field phone_number của bảng customer_information.
+- Hỏi về họ tên của khách hàng vào field name trong bảng customer_information.
+
+## Ràng buộc:
+- Chỉ đưa người dùng đến những link thông tin hướng dẫn sử dụng khi họ cần thông tin chi tiết. Không nên để chung trong 1 đoạn tin nhắn dài với các thông tin khác.
     """
 }
 
@@ -24,6 +46,9 @@ conditional_prompts = {
     """,
     "general_info_agent": """
     Giúp người dùng trả lời những câu hỏi liên quan đến Tính năng, Sản phẩm, Giải pháp, Bảng giá, Thông tin liên hệ của công ty,
+    """,
+    "software_packages_consulting_agent":"""
+    Giúp tư vấn cho người dùng về các gói phần mềm của công ty BigLead
     """
 }
 
@@ -33,7 +58,7 @@ monitor = Node(
     prompt_prefix=prompt_prefix_template["monitor"],
     tools=[],
     document_names=[],
-    next_nodes=["terms_and_conditions_agent", "general_info_agent"],
+    next_nodes=["terms_and_conditions_agent", "general_info_agent", "software_packages_consulting_agent"],
     conditional_prompt=conditional_prompts["monitor"]
 )
 
@@ -42,7 +67,7 @@ terms_and_conditions_agent = Node(
     prompt_prefix=prompt_prefix_template["terms_and_conditions_agent"],
     tools=[],
     document_names=["biglead_terms.txt"],
-    next_nodes=["monitor"],
+    next_nodes=[],
     conditional_prompt=conditional_prompts["terms_and_conditions_agent"]
 )
 
@@ -51,6 +76,15 @@ general_info_agent = Node(
     prompt_prefix=prompt_prefix_template["general_info_agent"],
     tools=[],
     document_names=["biglead_info.txt", "biglead_customer_services_multi-platform.txt"],
-    next_nodes=["monitor"],
+    next_nodes=[],
     conditional_prompt=conditional_prompts["general_info_agent"]
+)
+
+software_packages_consulting_agent = Node(
+    name="software_packages_consulting_agent",
+    prompt_prefix=prompt_prefix_template["software_packages_consulting_agent"],
+    tools=[],
+    document_names=["biglead_software_packages.txt"],
+    next_nodes=[],
+    conditional_prompt=conditional_prompts["software_packages_consulting_agent"]
 )
