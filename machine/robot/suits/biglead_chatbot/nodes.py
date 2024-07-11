4f53ebdf3af8962800dc2b7fa454e37698247105
nhan2003h <nhannhan888888@gmail.com>
2024-07-11 14:44:29 +0700

    Add suit for BigLead ChatBot

---
+++ machine/robot/suits/biglead_chatbot/nodes.py
@@ -0,0 +1,50 @@
+from machine.robot.engines.node import Node
+
+prompt_prefix_template = {
+    "monitor": """
+    Bạn là trợ lý thông minh tiếng Việt, hãy trả lời như một nhân viên tại BigLead về các câu hỏi liên quan đến công ty BigLead - một công ty hỗ trợ chăm sóc khách hàng đa kênh cho doanh nghiệp.
+    Bước 1. Hãy kiểm tra xem câu hỏi của người dùng có rõ ràng không, nếu không thì hãy bảo người dùng đặt lại câu hỏi.
+    Bước 2. Hãy kiểm tra câu hỏi người dùng có liên quan đến Tính năng, Sản phẩm, Giải pháp, Bảng giá, Liên hệ, Điều khoản, Thanh toán của công ty hay không, nếu không liên quan đến công ty thì hãy trả lời rằng câu hỏi này không liên quan, và yêu cầu người dùng đặt câu hỏi liên quan đến các mục trên.
+    """
+}
+
+
+conditional_prompts = {
+    "monitor": """
+    Giúp người dùng có được thông tin từ công ty BigLead.
+    """,
+    "terms_and_conditions_agent": """
+    Giúp người dùng trả lời những câu hỏi liên quan đến điều khoản sử dụng, hướng dẫn thanh toán của công ty.
+    """,
+    "general_info_agent": """
+    Giúp người dùng trả lời những câu hỏi liên quan đến Tính năng, Sản phẩm, Giải pháp, Bảng giá, Thông tin liên hệ của công ty,
+    """
+}
+
+
+monitor = Node(
+    name="monitor",
+    prompt_prefix=prompt_prefix_template["monitor"],
+    tools=[],
+    document_names=[],
+    next_nodes=["terms_and_conditions_agent", "general_info_agent"],
+    conditional_prompt=conditional_prompts["monitor"]
+)
+
+terms_and_conditions_agent = Node(
+    name="terms_and_conditions_agent",
+    prompt_prefix=prompt_prefix_template["terms_and_conditions_agent"],
+    tools=[],
+    document_names=["biglead_terms.txt"],
+    next_nodes=["monitor"],
+    conditional_prompt=conditional_prompts["terms_and_conditions_agent"]
+)
+
+general_info_agent = Node(
+    name="general_info_agent",
+    prompt_prefix=prompt_prefix_template["general_info_agent"],
+    tools=[],
+    document_names=["biglead_info.txt", "biglead_customer_services_multi-platform.txt"],
+    next_nodes=["monitor"],
+    conditional_prompt=conditional_prompts["general_info_agent"]
+)
